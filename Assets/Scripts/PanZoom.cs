e710f12865b15e9e37ddcd791c76ca4302c9aa65
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-03-29 15:38:35 +0100

    Added Zoom In and Zoom Out functionality

--- Assets/Scripts/PanZoom.cs
+++ Assets/Scripts/PanZoom.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PanZoom : MonoBehaviour
 {
-    private Camera cam;
-    Vector3 touchStart;
-    public float zoomOutMin = 1;
-    public float zoomOutMax = 8;
-	
-	// Update is called once per frame
-	void Update () {
+    public Camera cam;
+    private Vector3 touchStart;
+    public float zoomOutMin;
+    public float zoomOutMax;
+
+    private void Start()
+    {
+        Debug.Log(cam.name);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if(Input.GetMouseButtonDown(0)){
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            touchStart = cam.ScreenToWorldPoint(Input.mousePosition);
         }
         if(Input.touchCount == 2){
             Touch touchZero = Input.GetTouch(0);
@@ -28,13 +33,13 @@ public class PanZoom : MonoBehaviour
 
             zoom(difference * 0.01f);
         }else if(Input.GetMouseButton(0)){
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position += direction;
+            Vector3 direction = touchStart - cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position += direction;
         }
-        zoom(Input.GetAxis("Mouse ScrollWheel"));
+        zoom(Input.GetAxis("Mouse ScrollWheel") * 4f);
 	}
 
     void zoom(float increment){
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 }
