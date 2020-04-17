99706f7072034cafd4c76348fc02d77109904c58
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-04-17 13:13:42 +0100

    Reworked HIghlight.cs script to select planting areas properly

---
+++ Assets/Scripts/Camera Scripts/PanZoom.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanZoom : MonoBehaviour
+{
+    public Camera cam;
+    private Vector3 touchStart;
+    public float zoomOutMin;
+    public float zoomOutMax;
+    float xpos;
+    float zpos;
+
+    public Slider zoomSlider;
+
+    private void Start()
+    {
+        xpos = cam.transform.position.x;
+        zpos = cam.transform.position.z;
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+        if (Input.touchCount == 2){
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position += direction;          
+            
+        }
+
+        zoom(Input.GetAxis("Mouse ScrollWheel") * 4f);
+        zoomSlider.value = Camera.main.orthographicSize;
+	}
+
+    void zoom(float increment){
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
--- Assets/Scripts/Highlight.cs
+++ Assets/Scripts/Highlight.cs
@@ -4,24 +4,69 @@ using UnityEngine;
 
 public class Highlight : MonoBehaviour
 {
+    [SerializeField]private GameObject highlightedGrid;
+    //[SerializeField]private Material gridColour;
+    private List<GameObject> gridSqaures;
+    public LayerMask whatIsGrids;
+    public LayerMask whatIsPlots;
+    public bool selected;
+    public Color highlighted;
+    public Color unhighlighted;
+
     private Ray ray;
     private RaycastHit hit;
-    private Material gridPiece;
-    public LayerMask whatIsGrid;
 
-    void FixedUpdate()
+
+    private void Update()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, whatIsGrid))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, whatIsGrids))
         {
-            Color color;
-            gridPiece = hit.transform.gameObject.GetComponent<Renderer>().material;
-            color = hit.transform.gameObject.GetComponent<Renderer>().material.GetColor("_TintColor");
-            gridPiece.SetColor("_TintColor", new Color(1f, 1f, 1f, 1f));
+            highlightedGrid = hit.transform.gameObject;
+            foreach (Renderer renderer in highlightedGrid.GetComponentsInChildren<Renderer>())
+            {
+                renderer.material.SetColor("_TintColor", highlighted);
+            }
+
         }
         else
         {
-            gridPiece.SetColor("_TintColor", new Color(0.46f, 1f, 0f, 1f));
+            if(highlightedGrid != null)
+            {
+                foreach (Renderer renderer in highlightedGrid.GetComponentsInChildren<Renderer>())
+                {
+                    renderer.material.SetColor("_TintColor", unhighlighted);
+                }
+                highlightedGrid = null;
+            }
+           
         }
+
     }
+
+    private void FixedUpdate()
+    {
+        if(highlightedGrid != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!selected)
+                {
+                    selected = true;
+                    Debug.Log("Selected " + highlightedGrid.name);
+                }
+                else if(selected)
+                {
+                    selected = false;
+                    Debug.Log("Deselected " + highlightedGrid.name);
+                }
+                else
+                {
+                    Debug.Log("There is no grid selected");
+                }
+            }
+        }
+      
+    }
+
 }
